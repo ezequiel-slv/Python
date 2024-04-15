3656b663ae14d08e372e7c0d05e033621c79aadf
Ezequiel <ezequielsilvalima01@gmail.com>
2024-04-15 09:19:49 -0300

    commit

--- Capitulo 2/rev.py
+++ Capitulo 2/rev.py
@@ -1,10 +1,14 @@
 print("Revisão")
+exer1 = (35+35)
+print(f"35+35 = {exer1}")
+exer2 = (30-20)
+print(f"30-20 = {exer2}")
+exer3 = (10*5)
+print(f"10*5 = {exer3}")
+exer4 = (44/2)
+print(f"44/2 = {int(exer4)}")
+exer5 = (5**2)
+print(f"5**2 = {exer5}")
 
-exer1 = (10*5)
-print(f"50*5 = {exer1}")
-exer2 = (5**2)
-print(f"5**2 = {exer2}")
-exer3 = (44/2)
-print(f"44/2 = {int(exer3)}")
-
-
+print("expressões")
+expressao1
