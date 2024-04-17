d2da430b849faf438812af03103575bab038a41f
Ezequiel <ezequielsilvalima01@gmail.com>
2024-04-17 08:45:24 -0300

    commit

---
+++ Capitulo 3/revcap3.py
@@ -0,0 +1,24 @@
+#tipo de daddos 
+print("tipo de dados")
+num1 = 4 
+print(type(num1))
+num2 = 5.2
+print(type(num2))
+
+#bool
+print("Bool")
+a = 3
+b = 2
+c = 9
+d = 1
+
+print(a < b) #false
+print(c > a) #true
+print(d <= c) #true
+print(d != a) #true
+
+var1 = 5
+var2 = 7
+
+print(var1 > var2) #false
+
