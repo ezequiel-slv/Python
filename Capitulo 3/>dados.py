c6d562f2cb1db5ca5b9b6342650b9f15de7efb30
Ezequiel <ezequielsilvalima01@gmail.com>
2024-04-24 09:43:46 -0300

    commit

---
+++ Capitulo 3/>dados.py
@@ -0,0 +1,23 @@
+#entrada de dados
+
+#print("Entrada de dados")
+#x = float(input("Digite um valor: "))
+
+#print(x)
+
+nome = input("Digite seu nome: ")
+
+#print("Vocẽ digita %s" % nome)
+
+print("Olá, %s!" % nome)
+
+#Conversão de valores
+print('conversão de entrada de dados')
+
+anos = int(input("Anos de serviço: "))
+
+valor_por_ano = float(input("Valor por ano: "))
+
+bonus = anos * valor_por_ano
+
+print("Bônus de %5.2f" % bonus)
---
+++ Capitulo 3/sequ&temp.py
@@ -0,0 +1,19 @@
+# Sequência e tempo
+
+divida = 0 #1
+
+compra = 100 #2
+
+divida = divida + compra #3
+
+compra = 200 #4
+
+divida = divida + compra #5
+
+compra = 300 #6
+
+divida = divida + compra #7
+
+compra = 0 #8
+
+print(divida) #9
