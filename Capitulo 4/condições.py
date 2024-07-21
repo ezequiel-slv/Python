d03f983c765090753c545298eb9af44ad5de1837
Ezequiel <ezequielsilvalima01@gmail.com>
2024-07-21 13:08:57 -0300

    commit

--- Capitulo 4/condições.py
+++ Capitulo 4/condições.py
@@ -1,5 +1,6 @@
 #Formato da estrutura de condicional if
-'''
+
+#Listagem 4.2 – Condições
 a = int(input("Primeiro valor: "))
 
 b = int(input("Segundo valor: "))
@@ -15,6 +16,8 @@ if calculo == 2:
     print('Correto')
 else:
     print('Incorreto')
+    
+#Listagem 4.3 – Carro novo ou velho, dependendo da idade
 
 idade = int(input('Digite a idade do seu carro: '))
 
@@ -22,6 +25,8 @@ if idade <= 3:
     print('Novo')
 if idade > 3:
     print('Velho')
+    
+#Exercício 4.2 Escreva um programa que pergunte a velocidade do carro de um usuário. Caso ultrapasse 80 km/h, exiba uma mensagem dizendo que o usuário foi multado. Nesse caso, exiba o valor da multa, cobrando R$ 5 por km acima de 80 km/h.
 
 velocidade = int(input('Digite a velocidade do carro: '))
 
@@ -31,6 +36,8 @@ if velocidade > 80:
     print(f'Você recebeu uma multa no valor de: {multa}R$')
 if velocidade <= 80:
     print('Veiculo liberado')
+    
+#Exercício 4.3 Escreva um programa que leia três números e que imprima o maior e o menor.
 
 salario = float(input('Digite o valor do seu salário: '))
 base = salario
@@ -42,6 +49,8 @@ if base > 1000:
     imposto = imposto + ((base - 1000) * 0.20)
 print('Salário: R$%6.2f imposto a pagar: R$%6.2f' % (salario, imposto)) 
 
+#Exercício 4.4 Escreva um programa que pergunte o salário do funcionário e calcule o valor do aumento. Para salários superiores a R$ 1.250,00, calcule um aumento de 10%. Para os inferiores ou iguais, de 15%.
+
 num1 = int(input("Digite o primeiro valor: "))
 num2 = int(input("Digite o segundo valor: "))
 num3 = int(input("Digite o terceiro valor: "))
@@ -50,7 +59,7 @@ maxnum = max(num1, num2, num3)
 minnum = min(num1, num2, num3)
 
 print(f"O maior número e o menor número, respectivamente são: {maxnum} e {minnum}")
-'''
+
 #Exercício 4.5 Execute o programa (Listagem 4.5) e experimente alguns valores. Verifique se os resultados foram os mesmos do programa anterior 
 
 salario = int(input('Digite seu salário: '))
